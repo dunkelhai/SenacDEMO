43d27afc91c367498545a931925b54341487553a
Alysson Oliveira <3oalysson@gmail.com>
2024-06-17 21:53:27 -0300

    teste de commit

--- src/main/java/com/senac/senacdemo/HelloController.java
+++ src/main/java/com/senac/senacdemo/HelloController.java
@@ -7,6 +7,8 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
