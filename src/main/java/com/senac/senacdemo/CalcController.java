81942b2fc2c58c27c8f4f1a861a1fdaac8172c60
Alysson Oliveira <3oalysson@gmail.com>
2024-06-24 20:58:54 -0300

    ajustes

--- src/main/java/com/senac/senacdemo/CalcController.java
+++ src/main/java/com/senac/senacdemo/CalcController.java
@@ -11,11 +11,12 @@ public class CalcController {
     private TextArea numero1;
     @FXML
     private TextArea numero2;
-
-
-
+    
     @FXML
-    protected void onHelloButtonClick() {
-        resultado.setText("Welcome to JavaFX Application!");
+    protected void somar() {
+        double n1 = Double.parseDouble(numero1.toString());
+        double n2 = Double.parseDouble(numero2.toString());
+        double soma = n1 + n2;
+        resultado.setText("Resultado: " + soma);
     }
 }
