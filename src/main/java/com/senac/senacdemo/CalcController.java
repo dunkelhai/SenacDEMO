a4232350830b56b74e1d1ac110c8c2ebbf57563c
Alysson Oliveira <3oalysson@gmail.com>
2024-06-24 21:14:26 -0300

    ajustes

--- src/main/java/com/senac/senacdemo/CalcController.java
+++ src/main/java/com/senac/senacdemo/CalcController.java
@@ -2,21 +2,21 @@ package com.senac.senacdemo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class CalcController {
     @FXML
     private Label resultado;
     @FXML
-    private TextArea numero1;
+    private TextField numero1;
     @FXML
-    private TextArea numero2;
-    
+    private TextField numero2;
+
     @FXML
     protected void somar() {
-        double n1 = Double.parseDouble(numero1.toString());
-        double n2 = Double.parseDouble(numero2.toString());
-        double soma = n1 + n2;
+        Double n1 = Double.parseDouble(numero1.getText());
+        Double n2 = Double.parseDouble(numero2.getText());
+        Double soma = n1 + n2;
         resultado.setText("Resultado: " + soma);
     }
 }
