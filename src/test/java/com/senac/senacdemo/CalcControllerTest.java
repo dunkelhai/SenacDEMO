69a4d797f857493655ec855a90742fc8605929e4
Alysson Oliveira <3oalysson@gmail.com>
2024-07-01 18:43:46 -0300

    teste

---
+++ src/test/java/com/senac/senacdemo/CalcControllerTest.java
@@ -0,0 +1,12 @@
+package com.senac.senacdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcControllerTest {
+
+    @Test
+    void somar() {
+    }
+}
