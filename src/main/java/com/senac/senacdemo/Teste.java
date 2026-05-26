c95dc371264c1f7c8d9f12ee73385b12a92b5323
Alysson Oliveira <82451374+dunkelhai@users.noreply.github.com>
2026-05-25 21:47:03 -0300

    teste

--- src/main/java/com/senac/senacdemo/Teste.java
+++ src/main/java/com/senac/senacdemo/Teste.java
@@ -1,6 +1,13 @@
 package com.senac.senacdemo;
 
 public class Teste {
-  System.out.print("teste");
-}
+  public static void main(String[] args) {
+    System.out.println("Novo comentario ajustado");
+  }
 
+
+public static void salvaNome(String nome) {
+    System.out.println("teste" + nome);
+  }
+
+}
